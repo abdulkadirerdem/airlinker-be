df9ba48638d7839a05c7601a497696ab238f93ef
abdulkadirerdem <akadir_1256@hotmail.com>
2024-08-14 12:16:00 +0300

    fix: import text adjusted

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const { UserModel } = require("../api/models/Users");
+const { UserModel } = require("../api/models/users");
 
 passport.use(
   new GoogleStrategy(
